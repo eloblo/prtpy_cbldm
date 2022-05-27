2beb35a238553198a1892902f678c22f951bc9e9
eloblo <eloblo250@gmail.com>
2022-05-27 14:43:35 +0300

    added cbldm_bins

--- prtpy/partitioning/cbldm.py
+++ prtpy/partitioning/cbldm.py
@@ -92,89 +92,146 @@ def cbldm(
     if length == 0:  # empty items returns empty partition
         return bins
 
-    normalised_items = []  # list of bins, each bin contain a sub partition
-    for i in sorted_items:
-        b = BinsKeepingContents(2)
-        b.set_valueof(valueof)
-        b.add_item_to_bin(item=i, bin_index=1)
-        normalised_items.append(b)
-    alg = CBLDM_algo(length=length, time_in_seconds=time_in_seconds, len_delta=partition_difference, start=start, val=valueof)
+    normalised_items = cbldm_bins(sorted_items, valueof)
+    alg = CBLDM_algo(length=length, time_in_seconds=time_in_seconds, len_delta=partition_difference, start=start)
     alg.part(normalised_items)
     return alg.best
 
 
 class CBLDM_algo:
 
-    def __init__(self, length, time_in_seconds, len_delta, start, val):
+    def __init__(self, length, time_in_seconds, len_delta, start):
         self.sum_delta = np.inf  # partition sum difference
         self.length = length
-        self.time_in_seconds = time_in_seconds
+        self.time_in_seconds = np.inf
         self.len_delta = len_delta  # partition cardinal difference
         self.start = start
         self.best = BinsKeepingContents(2)
         self.best.add_item_to_bin(np.inf, 1)
         self.opt = False
-        self.val = val
 
     def part(self, items):
         if time.perf_counter() - self.start >= self.time_in_seconds or self.opt:
             return
-        if len(items) == 1:  # possible partition
-            if abs(len(items[0].bins[0]) - len(items[0].bins[1])) <= self.len_delta and abs(
-                    items[0].sums[0] - items[0].sums[1]) < self.sum_delta:
-                self.best = items[0]
-                self.sum_delta = abs(items[0].sums[0] - items[0].sums[1])
+        if items.get_length() == 1:  # possible partition
+            cur_bin = items.get_item(0)
+            cur_sum_delta = items.get_sum(cur_bin)
+            cur_len_delta = items.get_cardinal(cur_bin)
+            if cur_len_delta <= self.len_delta and cur_sum_delta < self.sum_delta:
+                self.best = cur_bin
+                self.sum_delta = cur_sum_delta
                 if self.sum_delta == 0:
                     self.opt = True
                 return
         else:
-            sum_xi = 0  # calculate the sum of the sum differences in items
-            max_x = 0  # max sum difference
-            for i in items:
-                xi = abs(i.sums[0] - i.sums[1])
-                sum_xi += xi
-                if xi > max_x:
-                    max_x = xi
-            if 2 * max_x - sum_xi >= self.sum_delta:
+            if 2 * items.max_x - items.sum_x >= self.sum_delta:
                 return
-            sum_mi = 0  # calculate the sum of the cardinal differences in items
-            max_m = 0  # max cardinal difference
-            for i in items:
-                mi = abs(len(i.bins[0]) - len(i.bins[1]))
-                sum_mi += mi
-                if mi > max_m:
-                    max_m = mi                                                      # despite being in the paper
-            if 2 * max_m - sum_mi > self.len_delta:  # or sum_mi < self.difference: # or breaks algorithm
-                return                                                              # breaks on [1,1,1,1,1,1,1,1,1,1]
-            if len(items) <= math.ceil(self.length / 2):
-                items = sorted(items, key=lambda item: abs(item.sums[0] - item.sums[1]), reverse=True)
-            # split items to left branch and right branch according to partition type
-            left = items[2:]
-            right = items[2:]
-            split = BinsKeepingContents(2)  # split partition according to sum of bins
-            combine = BinsKeepingContents(2)  # merge partition according to sum of bins
-            split.set_valueof(self.val)
-            combine.set_valueof(self.val)
+            # despite being in the paper, or condition breaks algorithm. breaks on [1,1,1,1,1,1,1,1,1,1]
+            if 2 * items.max_m - items.sum_m > self.len_delta:  # or items.sum_m < self.difference:
+                return
+            if items.get_length() <= math.ceil(self.length / 2):
+                items.sort()
 
-            for section in range(2):  # [small, big] + [small, big] -> [small + small, big + big]
-                for bin_i in range(2):
-                    for i in items[section].bins[bin_i]:
-                        combine.add_item_to_bin(i, bin_i)
-            combine.sort()
+            self.part(items.get_left())
+            self.part(items.get_right())
 
-            for i in items[0].bins[0]:  # [small, big] + [small, big] -> [small + big, small + big]
-                split.add_item_to_bin(i, 1)
-            for i in items[0].bins[1]:
-                split.add_item_to_bin(i, 0)
+
+class cbldm_bins:
+
+    def __init__(self, items, val):
+        self.val = val
+        self.items = []  # list of bins, each bin contain a sub partition
+        for i in items:
+            b = BinsKeepingContents(2)
+            b.set_valueof(self.val)
+            b.add_item_to_bin(item=i, bin_index=1)
+            self.items.append(b)
+
+        self.sum_m = 0
+        self.max_m = 0
+        self.sum_x = 0
+        self.max_x = 0
+        for i in self.items:
+            xi = self.get_sum(i)
+            mi = self.get_cardinal(i)
+            self.sum_x += xi
+            self.sum_m += mi
+            if xi > self.max_x:
+                self.max_x = xi
+            if mi > self.max_m:
+                self.max_m = mi
+
+    def get_item(self, index):
+        return self.items[index]
+
+    def get_cardinal(self, b):
+        return abs(len(b.bins[0]) - len(b.bins[1]))
+
+    def get_sum(self, b):
+        return abs(b.sums[0] - b.sums[1])
+
+    def get_length(self):
+        return len(self.items)
+
+    def sort(self):
+        self.items = sorted(self.items, key=lambda item: abs(item.sums[0] - item.sums[1]), reverse=True)
+
+    def get_left(self):
+        left = cbldm_bins([], self.val)
+        new_items = self.items[2:]
+        split = BinsKeepingContents(2)
+        split.set_valueof(self.val)
+        for i in self.items[0].bins[0]:  # [small, big] + [small, big] -> [small + big, small + big]
+            split.add_item_to_bin(i, 1)
+        for i in self.items[0].bins[1]:
+            split.add_item_to_bin(i, 0)
+        for bin_i in range(2):
+            for i in self.items[1].bins[bin_i]:
+                split.add_item_to_bin(i, bin_i)
+        split.sort()
+        new_items.append(split)
+        left.items = new_items
+        left.sum_x = self.sum_x - self.get_sum(self.items[0]) - self.get_sum(self.items[1]) + self.get_sum(split)
+        left.sum_m = self.sum_m - self.get_cardinal(self.items[0]) - self.get_cardinal(self.items[1]) + self.get_cardinal(split)
+        if self.max_x == self.get_sum(self.items[0]) or self.max_x == self.get_sum(self.items[1]):
+            if self.get_length() >= 3:
+                left.max_x = max(self.get_sum(split), self.get_sum(self.items[2]))
+            else:
+                left.max_x = self.get_sum(split)
+        else:
+            left.max_x = max(self.get_sum(split), self.max_x)
+        if self.max_m != self.get_cardinal(self.items[0]) and self.max_m != self.get_cardinal(self.items[1]):
+            left.max_m = max(self.get_cardinal(split), self.max_m)
+        else:
+            left.max_m = max(self.get_cardinal(i) for i in new_items)
+        return left
+
+    def get_right(self):
+        right = cbldm_bins([], self.val)
+        new_items = self.items[2:]
+        combine = BinsKeepingContents(2)
+        combine.set_valueof(self.val)
+        for section in range(2):  # [small, big] + [small, big] -> [small + small, big + big]
             for bin_i in range(2):
-                for i in items[1].bins[bin_i]:
-                    split.add_item_to_bin(i, bin_i)
-            split.sort()
-
-            right.append(combine)
-            left.append(split)
-            self.part(left)
-            self.part(right)
+                for i in self.items[section].bins[bin_i]:
+                    combine.add_item_to_bin(i, bin_i)
+        combine.sort()
+        new_items.append(combine)
+        right.items = new_items
+        right.sum_x = self.sum_x - self.get_sum(self.items[0]) - self.get_sum(self.items[1]) + self.get_sum(combine)
+        right.sum_m = self.sum_m - self.get_cardinal(self.items[0]) - self.get_cardinal(self.items[1]) + self.get_cardinal(combine)
+        if self.max_x == self.get_sum(self.items[0]) or self.max_x == self.get_sum(self.items[1]):
+            if self.get_length() >= 3:
+                right.max_x = max(self.get_sum(combine), self.get_sum(self.items[2]))
+            else:
+                right.max_x = self.get_sum(combine)
+        else:
+            right.max_x = max(self.get_sum(combine), self.max_x)
+        if self.max_m != self.get_cardinal(self.items[0]) and self.max_m != self.get_cardinal(self.items[1]):
+            right.max_m = max(self.get_cardinal(combine), self.max_m)
+        else:
+            right.max_m = max(self.get_cardinal(i) for i in new_items)
+        return right
 
 
 if __name__ == "__main__":
